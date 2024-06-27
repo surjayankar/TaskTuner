edc4fae6c32acd1629082bb596d4d20012d39ffb
surjayan kar <surjayan.smartboy@gmail.com>
2024-06-27 17:24:55 +0530

    Added Navbar and Sidebar with Profile Avatar

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@
 import { Button } from "@/components/ui/button";
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
-import { signInActiom } from "@/actions/auth-action";
+import {  signInAction } from "@/actions/auth-action";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Login</h1>
-      <form action={signInActiom}>
+      <form action={signInAction}>
         <Button>
           Log In
         </Button>
